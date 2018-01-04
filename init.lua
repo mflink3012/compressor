e5d0c7477da333ab117d26ffd615bb5b80112c87
Marco Link <mfl-github@bitblox.de>
2018-01-04 10:36:33 +0100

    First working version.

---
+++ init.lua
@@ -0,0 +1,11 @@
+minetest.register_craft({
+  type = "cooking",
+  output = "default:diamond",
+  recipe = "default:coal_block",
+})
+
+minetest.register_craft({
+  type = "cooking",
+  output = "default:mese_crystal_fragment",
+  recipe = "default:obsidian_glass",
+})
